e570d1a308441a275ad163fc21515366aeee5650
Mudassir Ali <mudassirali36@gmail.com>
2021-01-23 23:01:41 +0500

    initial commit

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,61 +1,102 @@
 import { BigInt } from "@graphprotocol/graph-ts"
-import {
-  Marketplace,
-  Approval,
-  Transfer
-} from "../generated/Marketplace/Marketplace"
-import { ExampleEntity } from "../generated/schema"
+import { ProductAdded, ProductClosed, ProductRemoved, OfferAdded, OfferDecided, OfferPaymentSentInEther, OfferPaymentSentInToken } from "../generated/Marketplace/Marketplace"
+import { Product, Offer, ProductDeleted, PaymentInEther, PaymentInToken, ProductOff, OfferDone } from "../generated/schema"
 
-export function handleApproval(event: Approval): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+export function handleProductAdded(event: ProductAdded): void {
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (entity == null) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
+  let product = Product.load(event.params.productId.toHex());
 
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
+  if (product == null) {
+    product = new Product(event.params.productId.toHex());
   }
+    product.id = event.params.productId.toHex();
+    product.name = event.params.productName;
+    product.price = event.params.price;
+    product.deadline = event.params.deadline;
+    product.owner = event.params.orderOwner;
+    product.token = event.params.token;
+    product.orderStage = event.params.status;
+    product.quantity = event.params.quantity;
 
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity.owner = event.params.owner
-  entity.spender = event.params.spender
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.approve(...)
-  // - contract.decreaseAllowance(...)
-  // - contract.increaseAllowance(...)
-  // - contract.transfer(...)
-  // - contract.transferFrom(...)
-  // - contract.allowance(...)
-  // - contract.balanceOf(...)
-  // - contract.decimals(...)
-  // - contract.name(...)
-  // - contract.symbol(...)
-  // - contract.totalSupply(...)
+    product.save()
 }
 
-export function handleTransfer(event: Transfer): void {}
+export function handleOfferAdded(event: OfferAdded): void {
+  // let product = Product.load(event.params.productId.toHex()); can be used
+  let offer = Offer.load(event.params.offerId.toHex());
+
+  if (offer == null) {
+    offer = new  Offer(event.params.offerId.toHex());
+  }
+    offer.productId = event.params.productId.toHex();
+    offer.price = event.params.price;
+    offer.quantity = event.params.quantity;
+    offer.owner = event.params.offerMaker;
+
+    offer.save();
+}
+
+export function  handleOfferDecided(event: OfferDecided): void {
+    let offerDecided = OfferDone.load(event.params._event.transaction.hash.toHex());
+
+    if (offerDecided == null) {
+      offerDecided = new OfferDone(event.params._event.transaction.hash.toHex());
+    }
+    offerDecided.productId = event.params.productId.toHex();
+    offerDecided.acceptedOfferId = event.params.offerId.toHex();
+    offerDecided.offerMaker = event.params.offerMaker;
+
+    offerDecided.save()
+}
+
+export function  handleProductRemoved(event: ProductRemoved): void {
+  let productDeleted = ProductDeleted.load(event.params._event.transaction.hash.toHex());
+
+    if (productDeleted == null) {
+      productDeleted = new ProductDeleted(event.params._event.transaction.hash.toHex());
+    }
+    productDeleted.id = event.params.productId.toHex();
+    productDeleted.save();
+}
+
+export function  handleOfferPaymentSentInEther(event: OfferPaymentSentInEther): void {
+  let paymentInEther = PaymentInEther.load(event.params._event.transaction.hash.toHex());
+
+  if (paymentInEther == null) {
+    paymentInEther = new PaymentInEther(event.params._event.transaction.hash.toHex());
+  }
+    paymentInEther.productId = event.params.productId.toHex();
+    paymentInEther.offerId = event.params.offerId.toHex();
+    paymentInEther.amount = event.params.amount;
+    paymentInEther.beneficiary = event.params.beneficiary;
+    paymentInEther.offerMaker = event.params.offerMaker;
+
+    paymentInEther.save();
+}
+
+export function handleOfferPaymentSentInToken(event: OfferPaymentSentInToken): void {
+  let paymentInToken = PaymentInToken.load(event.params._event.transaction.hash.toHex());
+
+  if (paymentInToken == null) {
+    paymentInToken = new PaymentInToken(event.params._event.transaction.hash.toHex());
+  }
+    paymentInToken.productId = event.params.productId.toHex();
+    paymentInToken.offerId = event.params.offerId.toHex();
+    paymentInToken.amount = event.params.amount;
+    paymentInToken.beneficiary = event.params.beneficiary;
+    paymentInToken.offerMaker = event.params.offerMaker;
+
+    paymentInToken.save();
+}
+
+export function handleProductClosed(event: ProductClosed): void {
+  let productClosed = ProductOff.load(event.params._event.transaction.hash.toHex());
+
+  if (productClosed == null) {
+    productClosed = new ProductOff(event.params._event.transaction.hash.toHex());
+  }
+  productClosed.productId = event.params.productId.toHex();
+  productClosed.save();
+}
+
+
